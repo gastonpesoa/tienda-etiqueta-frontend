a239a7c7af06b279c180646261d57db2a8a87c4b
gastonpesoa <gastonpesoa@gmail.com>
2022-10-31 10:02:35 -0300

    .

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,32 +1,52 @@
 import { useState } from 'react';
 import { useParams } from 'react-router'
 import { useNavigate } from 'react-router-dom'
-import { Col, Row, Typography, Image, Space, Radio, Card, Button } from 'antd';
+import { Col, Row, Typography, Image, Space, Radio, Card, Button, Tabs, Badge } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import Price from "../components/Price";
 import UnitsSelect from "../components/UnitsSelect";
 import Rating from '../components/Rating';
 import myData from '../data.json';
-const { Title, Text } = Typography;
+const { Title, Text, Paragraph } = Typography;
 
 const ProductDetail = () => {
-
-    const { trajes } = myData;
-    const product = trajes[id - 1];
-
-    const tabs = [
-        { label: 'Descripción', key: 'item-1', children: 'Content 1' },
-        { label: 'Opiniones', key: 'item-2', children: 'Content 2' },
-      ];
 
     const { id } = useParams();
     const navigate = useNavigate();
     const [unit, setUnit] = useState(1);
-    
+
+    const { trajes } = myData;
+    const product = trajes[id - 1];
+
     const onSizeChange = (e) => {
         console.log('radio checked', e.target.value);
     };
-    
+
+    const tabs = [
+        {
+            label: <Text>Descripción</Text>,
+            key: 'item-1',
+            children:
+                <>
+                    <Title level={5}>Detalle</Title>
+                    <Paragraph>
+                        {product.detail}
+                    </Paragraph>
+                </>
+        },
+        {
+            label:
+                <Space>
+                    <Text>Opiniones</Text>
+                    <Badge style={{ color: '#6A983C', backgroundColor: '#F4F8EC' }} count={product.reviews.length} />
+                </Space>,
+            key: 'item-2',
+            children:
+                <>
+                </>
+        },
+    ];
+
     return (
         <>
             <Row gutter={16}>
@@ -127,7 +147,7 @@ const ProductDetail = () => {
                             }
                         </Col>
                     </Row>
-                    <Row>
+                    <Row style={{ marginBottom: '60px' }}>
                         <Col span={24}>
                             <Card>
                                 <Row gutter={16}>
@@ -146,6 +166,11 @@ const ProductDetail = () => {
                                     </Col>
                                 </Row>
                             </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <Tabs items={tabs} />
                         </Col>
                     </Row>
                 </Col>
