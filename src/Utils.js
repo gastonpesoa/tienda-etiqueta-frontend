20b8eaba691d37cfbfd04275a2d7e69722be8061
Ezequiel Mahafud <emahafud@provincianet.com.ar>
2022-12-04 00:29:48 -0300

    Pantalla de detalles de orden para empleado

--- src/Utils.js
+++ src/Utils.js
@@ -2,25 +2,25 @@ export const formatState = (state) => {
    let stateFormated = { color: '', text: '', description: '' }
    switch (state) {
       case 'CONFIRMADA':
-         stateFormated = { color: '#40a9ff', text: 'Confirmada', description: 'Realizaste la compra el' };
+         stateFormated = { color: '#40a9ff', text: 'Confirmada', description: 'Realizaste la compra el', description_seller: 'Compra realizada el' };
          break;
       case 'LISTA_PARA_RETIRAR':
-         stateFormated = { color: '#85a5ff', text: 'Lista para retirar', description: 'Tu compra esta lista para que la retires por sucursal desde el' };
+         stateFormated = { color: '#85a5ff', text: 'Lista para retirar', description: 'Tu compra está lista para que la retires por sucursal desde el', description_seller: 'Compra lista para ser retirada por sucursal el' };
          break;
       case 'LISTA_PARA_ENTREGAR':
-         stateFormated = { color: '#bae637', text: 'Lista para entregar', description: 'Tu compra esta lista para ser entregada desde el' };
+         stateFormated = { color: '#bae637', text: 'Lista para entregar', description: 'Tu compra está lista para ser entregada desde el', description_seller: 'Compra lista para ser entregada desde el' };
          break;
       case 'EN_VIAJE':
-         stateFormated = { color: '#73d13d', text: 'En viaje', description: 'Tu compra esta en viaje desde el' };
+         stateFormated = { color: '#73d13d', text: 'En viaje', description: 'Tu compra está en viaje desde el', description_seller: 'Compra en viaje desde el' };
          break;
       case 'ENTREGADA':
-         stateFormated = { color: 'green', text: 'Entregada', description: 'Recibiste la compra el' };
+         stateFormated = { color: 'green', text: 'Entregada', description: 'Recibiste la compra el', description_seller: 'Compra entregada el' };
          break;
       case 'ENTREGA_FALLIDA':
-         stateFormated = { color: '#ffec3d', text: 'Entrega fallida', description: 'Intentamos entregar la compra el' };
+         stateFormated = { color: '#ffec3d', text: 'Entrega fallida', description: 'Intentamos entregar la compra el', description_seller: 'Intento de entrega fallido el' };
          break;
       case 'CANCELADA':
-         stateFormated = { color: 'volcano', text: 'Cancelada', description: 'Tu compra fué cancelada el' };
+         stateFormated = { color: 'volcano', text: 'Cancelada', description: 'Tu compra fué cancelada el', description_seller: 'Compra cancelada el' };
          break;
       default:
          break;
