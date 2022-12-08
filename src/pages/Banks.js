3e51721d79c47ab4069de785f5930b6d54af467f
pesoagaston <gastonpesoa@gmail.com>
2022-12-08 19:38:00 -0300

    abm bank

--- src/pages/Banks.js
+++ src/pages/Banks.js
@@ -1,94 +1,288 @@
-import { useNavigate } from 'react-router-dom'
-import { Row, Col, Button, Typography, Table } from 'antd';
+import { Space, Row, Col, Button, Typography, Table, Form, message, notification, Popconfirm, Input, InputNumber, Switch } from 'antd';
 import { useEffect, useState } from 'react';
 const { Title, Text } = Typography;
 
-const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-];
-  
-const columns = [
-    {
-      title: 'Banco',
-      dataIndex: 'bank',
-      key: 'bank',
-    },
-    {
-      title: 'Descuento',
-      dataIndex: 'discount',
-      key: 'discount',
-      render: (discount) => {
-        if (discount <= 0)
-            return '-';
-        return discount + '%';
-      }
-    },
-    {
-      title: 'Promoción vigente',
-      dataIndex: 'discount_status',
-      key: 'discount_status',
-      render: (discount_status) => {
-        return discount_status ? "Sí" : "No"
-      }
-    },
-];
-
 const Banks = () => {
 
-    const navigate = useNavigate()
-
     const [dataSource, setDataSource] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [showForm, setShowForm] = useState(false);
+    const [isCreateForm, setIsCreateForm] = useState(false);
+    const [updateId, setUpdateId] = useState('');
+    const [form] = Form.useForm();
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL_BASE}/banks/`)
-            .then((res) => res.ok ? res.json() : Promise.reject(res))
-            .then(({data}) => {
-                setDataSource(data);
-                /*if (data.length > 0) {
-                    data.forEach((bk) => {
-                        let bankAux = {};
-                        bankAux.value = bk.bank;
-                        bankAux.label = bk.bank;
-                        bankAux.discount = bk.discount_status ? bk.discount : 0;
-                        if (bk.discount !== null && bk.discount > 0 && bk.discount_status) {
-                            bankAux.label += ' (-' + bk.discount + '%)' ;
-                        }
-                        bankList.push(bankAux);
-                    });
-                } else {
-                    message.error("No hay bancos disponibles");
-                }*/
-            })
-            .catch((err) => {
-                console.error(err);
-                //message.error("Hubo un error al traer el listado de bancos disponibles, intente nuevamente más tarde");
-            });
+        getBanks(`${process.env.REACT_APP_API_URL_BASE}/banks`)
     }, []);
 
-    const handleClseSesion = () => {
-        navigate('/')
+    const getBanks = async (url) => {
+        setLoading(true);
+        try {
+            const res = await fetch(url, {
+                headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` }
+            })
+            const data = await res.json();
+            let newArray = []
+            data.data.map(item => {
+                newArray.push({
+                    key: item.id,
+                    bank: item.bank,
+                    discount: item.discount,
+                    discount_status: item.discount_status
+                })
+            })
+            setDataSource(newArray);
+            setLoading(false);
+        } catch (error) {
+            message.error(error)
+            console.log(error)
+            setLoading(false)
+        }
     }
+
+    const handleUpdateBank = (bank) => {
+        console.log("update bank", bank)
+        form.setFieldsValue({
+            bank: bank.bank,
+            discount: bank.discount,
+            discount_status: bank.discount_status
+        });
+        setUpdateId(bank.key);
+        setIsCreateForm(false);
+        setShowForm(true)
+    }
+
+    const handleCreateBank = () => {
+        setShowForm(true);
+        setIsCreateForm(true);
+        form.resetFields();
+    }
+
+    const handleDeleteBank = async (bank) => {
+        console.log("delete", bank)
+        try {
+            const res = await fetch(`${process.env.REACT_APP_API_URL_BASE}/banks/id/${bank.key}`, {
+                method: "DELETE",
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`,
+                    "Content-Type": "application/json"
+                }
+            })
+            const data = await res.json();
+            message.success("Banco eliminado con éxito!")
+            getBanks(`${process.env.REACT_APP_API_URL_BASE}/banks`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const cancel = (e) => {
+        console.log(e)
+    };
+
+    const onFinish = (values) => {
+        console.log("form finish", values)
+        if(values.discount_status === undefined){
+            values.discount_status = false
+        }
+        if (isCreateForm) {
+            registerBank(values)
+        } else {
+            updateBankData(values)
+        }
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+        errorInfo.errorFields.map((e) => {
+            openNotificationWithIcon(e.errors[0])
+        })
+    };
+
+    const openNotificationWithIcon = (message) => {
+        notification['error']({
+            message: 'Cargue los datos necesarios',
+            description: message,
+        });
+    };
+
+    const registerBank = (value) => {
+        fetch(`${process.env.REACT_APP_API_URL_BASE}/banks`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(value)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    message.error(data.message)
+                    console.log(data)
+                } else {
+                    message.success(`Banco registrada con éxito!`)
+                    getBanks(`${process.env.REACT_APP_API_URL_BASE}/banks`)
+                }
+            })
+    }
+
+    const updateBankData = async (values) => {
+        try {
+            const res = await fetch(`${process.env.REACT_APP_API_URL_BASE}/banks/id/${updateId}`, {
+                method: "PUT",
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(values)
+            })
+            const data = await res.json();
+            message.success("Las modificaciones han sido registradas con éxito!")
+            getBanks(`${process.env.REACT_APP_API_URL_BASE}/banks`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const columns = [
+        {
+            title: 'Banco',
+            dataIndex: 'bank',
+            key: 'bank',
+        },
+        {
+            title: 'Descuento',
+            dataIndex: 'discount',
+            key: 'discount',
+            render: (discount) => {
+                if (discount <= 0)
+                    return '-';
+                return discount + '%';
+            }
+        },
+        {
+            title: 'Promoción vigente',
+            dataIndex: 'discount_status',
+            key: 'discount_status',
+            render: (discount_status) => {
+                return discount_status ? "Sí" : "No"
+            }
+        },
+        {
+            title: 'Acciones',
+            render: (_, record) => (
+                <Space key={record._id}>
+                    <Button type='primary' onClick={() => { handleUpdateBank(record) }}>
+                        Modificar
+                    </Button>
+                    <Popconfirm
+                        title="Estás seguro que deseas eliminar este banco?"
+                        onConfirm={() => { handleDeleteBank(record) }}
+                        onCancel={cancel}
+                        okText="Si"
+                        cancelText="No"
+                    >
+                        <Button danger>
+                            Eliminar
+                        </Button>
+                    </Popconfirm>
+                </Space>
+            )
+        }
+    ];
 
     return (
         <>
-            <Title level={2}>Bancos</Title>
-            <Table
-                dataSource={dataSource}
-                columns={columns}
-            />
+            {
+                loading
+                    ? <Space active />
+                    : <>
+                        <Row>
+                            <Col span={12}>
+                                <Title level={2}>Gestión de Bancos</Title>
+                            </Col>
+                            <Col span={12} style={{ textAlign: 'right' }}>
+                                <Button type='primary' onClick={handleCreateBank}>Crear banco</Button>
+                            </Col>
+                        </Row>
+                        <Table dataSource={dataSource} columns={columns} />
+                        {
+                            showForm &&
+                            <Form
+                                labelCol={{ span: 24 }}
+                                wrapperCol={{ span: 24 }}
+                                layout="vertical"
+                                onFinish={onFinish}
+                                onFinishFailed={onFinishFailed}
+                                form={form}
+                            >
+                                <Row gutter={8}>
+                                    <Col span={8}>
+                                        <Form.Item
+                                            label="Nombre"
+                                            name="bank"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Por favor ingresá el nombre!',
+                                                },
+                                            ]}
+                                        >
+                                            <Input placeholder="Nombre" />
+                                        </Form.Item>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Form.Item
+                                            label="Porcentaje de descuento"
+                                            name="discount"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Por favor ingresá el descuento!',
+                                                },
+                                            ]}
+                                        >
+                                            <InputNumber placeholder="Porcentaje de descuento" style={{ width: 200 }} />
+                                        </Form.Item>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Form.Item
+                                            label="Activo"
+                                            name="discount_status"
+                                            valuePropName="checked"
+                                        >
+                                            <Switch />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                                <Row gutter={8}>
+                                    <Col span={24}>
+                                        <Button
+                                            type="primary"
+                                            htmlType="submit"
+                                        >
+                                            Guardar modificaciones
+                                        </Button>
+                                    </Col>
+                                </Row>
+                                <Row style={{ marginTop: 24 }}>
+                                    <Col span={24}>
+                                        <Button
+                                            danger
+                                            onClick={() => setShowForm(false)}
+                                        >
+                                            Cancelar
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        }
+                    </>
+            }
         </>
-        
+
     )
 }
 
